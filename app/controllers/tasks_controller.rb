3b7bb561d4d0683f2b7d7f9cad75583aad5fc336
Emilie Deschamps-Wright <e.wright@hotmail.fr>
2018-08-14 15:44:02 +0200

    Created the task crud

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,46 @@
 class TasksController < ApplicationController
+  before_action :find, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(update_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def update_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find
     @task = Task.find(params[:id])
   end
 end
